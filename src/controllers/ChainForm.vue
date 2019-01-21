243f29e771964db9b8a254603a1b710f3a7d3bb1
lickey777 <lickey_lc@163.com>
2019-01-21 17:30:41 +0800

    update src/controllers/ChainForm

--- src/controllers/ChainForm.vue
+++ src/controllers/ChainForm.vue
@@ -26,12 +26,12 @@
         :rules="messageHeaderRules"
       ></v-text-field>
       <h2>Consensus</h2>
-      <label>Algorithm(only support PoA now) *</label>
+      <label>Algorithm (only support PoA now) *</label>
       <v-select
         v-model="alg"
         :items="[{text: 'PoA', value: 'PoA'}]"
       ></v-select>
-      <label>Miner list *</label>
+      <label>Miner list (one or serveral addresses seperated by ,)*</label>
       <v-text-field
         v-model="minerList"
         :rules="[this.fieldRequired]"
@@ -52,11 +52,6 @@
         v-model="defaultPort"
         :rules="[this.fieldRequired,this.isPositiveInt,this.portLimit]"
       ></v-text-field>
-      <label>Rpc prot *</label>
-      <v-text-field
-        v-model="rpcPort"
-        :rules="[this.fieldRequired,this.isPositiveInt,this.portLimit]"
-      ></v-text-field>
       <label>Dns seed</label>
       <v-text-field
         v-model="dnsSeed"
@@ -71,7 +66,7 @@
         v-model="initReward"
         :rules="[this.fieldRequired,this.isPositiveInt]"
       ></v-text-field>
-      <label>Halving interval (an integer > 0) * </label>
+      <label>Halving interval (an integer >= 0, and 0 means no halving, which will reward miners constantly) *</label>
       <v-text-field
         v-model="halvingInterval"
         :rules="[this.fieldRequired,this.isPositiveInt]"
@@ -84,7 +79,7 @@
     </v-form>
     <v-card-actions>
       <v-spacer></v-spacer>
-      <v-btn color="success" @click="submit" :disabledx="!valid">Submit</v-btn>
+      <v-btn color="success" @click="submit" :disabled="!valid">Submit</v-btn>
     </v-card-actions>
   </v-card>
 </template>
@@ -92,20 +87,43 @@
 <script>
   import utils from '@/libs/utils';
 
+  let back = "";
+  for (var i=0;i<5;i++)
+  {
+    back += Math.floor(Math.random()*10);
+  }
+
+  function message_header()
+  {
+    var x = "",
+      arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+    for(var i=0; i<8; i++){
+      x += arr[Math.floor(Math.random()*16)]
+    }
+    return x;
+  }
+
+  var defaultChainId = "mychain" + back;
+  var defaultTokenName = "MYT" + back;
+  var defaultDes = "my chain " + back;
+  var defaultMessageHeader = message_header();
+  var defaultPort = Math.floor(Math.random()*(65534 - 1025 + 1) + 1025)
+  var defaultRpcPort = Math.floor(Math.random()*(65534 - 1025 + 1) + 1025)
+
   export default {
     data() {
       return {
         valid: false,
-        chainId: '',
-        tokenName: '',
-        description: '',
-        messageHeader: '',
+        chainId: defaultChainId,
+        tokenName: defaultTokenName,
+        description: defaultDes,
+        messageHeader: defaultMessageHeader,
         alg: 'PoA',
         minerList: '',
         blockInterval: 10,
         blockTimeout: 5,
-        defaultPort: '',
-        rpcProt: '',
+        defaultPort: defaultPort,
+        rpcPort: defaultRpcPort,
         dnsSeed: '',
         ipSeed: '',
         initReward: 2000000,
@@ -121,7 +139,7 @@
         ],
         messageHeaderRules: [
           this.fieldRequired,
-          v => /^[0-9a-fA-F]{8}$/.test(v) || 'Message header is 4 bytes in hex format',
+          v => /^[0-9a-f]{8}$/.test(v) || 'Message header is 4 bytes in hex format with "a-f" and "0-9",ignore "0x"',
         ],
       };
     },
@@ -130,12 +148,16 @@
         return !!v || 'This field is required';
       },
       isPositiveInt(v) {
-        return /^[1-9][0-9]*$/.test(v) || 'This field must be a positive integer';
+        return /^[0-9][0-9]*$/.test(v) || 'This field must be a positive integer';
       },
       portLimit(v) {
         return (v <= 65535 && v >= 1024) || 'This field is limited 1024-65535';
       },
       async submit() {
+        let halvingtime = 0;
+        if(this.halvingInterval !== 0){
+          halvingtime = this.halvingTimes;
+        }
         const res = (await utils.postData('/api/chain/launch', {
           chainId: this.chainId,
           tokenName: this.tokenName,
@@ -149,7 +171,7 @@
           ipSeed: this.ipSeed,
           initReward: this.initReward,
           halvingInterval: this.halvingInterval,
-          halvingTimes: this.halvingTimes,
+          halvingTimes: halvingtime,
         }));
         if (res && res.chainId) {
           this.$router.push({ name: 'Chain/View', query: { chainId: res.chainId } });
